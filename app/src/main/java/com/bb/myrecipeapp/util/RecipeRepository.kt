bd79a1b66baeb7cb047ae39dd4753dbb586540ed
Field Employee <fieldemployee@Fields-MacBook-Pro-4.local>
2020-04-16 15:20:38 -0400

    updated repository, view model

--- app/src/main/java/com/bb/myrecipeapp/util/RecipeRepository.kt
+++ app/src/main/java/com/bb/myrecipeapp/util/RecipeRepository.kt
@@ -2,12 +2,14 @@ package com.bb.myrecipeapp.util
 
 import com.bb.myrecipeapp.model.Ingredient
 import com.bb.myrecipeapp.model.Instruction
+import com.bb.myrecipeapp.model.Recipe
 import io.reactivex.Observable
 
 class RecipeRepository() {
 
         var cachedIngredient = emptyList<Ingredient>()
         var cachedInstruction = emptyList<Instruction>()
+        var cachedRecipe = emptyList<Recipe>()
 
         fun getIngredients(): Observable<List<Ingredient>> {
             if (cachedIngredient.isEmpty()) {
@@ -16,6 +18,25 @@ class RecipeRepository() {
             else {
                 return Observable.just(cachedIngredient)
                     .mergeWith(getIngredients())
+            }
+        }
+        fun getInstructions(): Observable<List<Instruction>> {
+            if (cachedInstruction.isEmpty()) {
+                return getInstructions()
+            }
+            else {
+                return Observable.just(cachedInstruction)
+                    .mergeWith(getInstructions())
+            }
+        }
+
+        fun getRecipes(): Observable<List<Recipe>> {
+            if (cachedRecipe.isEmpty()) {
+                return getRecipes()
+            }
+            else {
+                return Observable.just(cachedRecipe)
+                    .mergeWith(getRecipes())
             }
         }
     }
